a1676badde6c5de35bc267ed72260c6bec2d2c16
Steve Grehl <git@steve-grehl.de>
2022-06-16 11:10:56 +0200

    updateted Github actions, formatting

--- xlum/data/classes.py
+++ xlum/data/classes.py
@@ -8,15 +8,17 @@ from functools import cached_property
 
 from data.enumerations import CurveType, RecordType, State, SampleCondition
 
-class Xlum_DataFrame_Support(object):
 
+class Xlum_DataFrame_Support(object):
     def __init__(self) -> None:
         """derault construktor - NOT IMPLEMENTED
 
         Raises:
             NotImplementedError: not intended to instantiate
-        """        
-        raise NotImplementedError("This class cannot be instantiated, please implement your own class that inherit its functionalities.")
+        """
+        raise NotImplementedError(
+            "This class cannot be instantiated, please implement your own class that inherit its functionalities."
+        )
 
     @cached_property
     def df(self) -> pd.DataFrame:
@@ -26,43 +28,66 @@ class Xlum_DataFrame_Support(object):
         """
         # Find class attribute with nested list, eg records within a sequence
         for k in self.__dict__:
-            k:str
-            if len(k)>3 and k[:3] == "lst":
+            k: str
+            if len(k) > 3 and k[:3] == "lst":
                 strLabel = k
         # Create data frame with all class-attributes, including and renaming _meta (if available)
         df = pd.DataFrame([self]).drop(columns=[strLabel], axis="columns")
-        df.rename(columns={k: self.__class__.__name__+"."+k for k in df.columns}, inplace=True)
+        df.rename(
+            columns={k: self.__class__.__name__ + "." + k for k in df.columns},
+            inplace=True,
+        )
         lstDataFrames = [df]
         if "_meta" in self.__dict__:
-            lstDataFrames[0].drop(columns=self.__class__.__name__+"."+"_meta", axis="columns", inplace=True)
-            lstDataFrames.append(pd.DataFrame([self._meta]).rename(columns={c: self.__class__.__name__+"."+c for c in self._meta.__dict__}))
+            lstDataFrames[0].drop(
+                columns=self.__class__.__name__ + "." + "_meta",
+                axis="columns",
+                inplace=True,
+            )
+            lstDataFrames.append(
+                pd.DataFrame([self._meta]).rename(
+                    columns={
+                        c: self.__class__.__name__ + "." + c
+                        for c in self._meta.__dict__
+                    }
+                )
+            )
 
         # Add nested list as rows, records in a sequence
-        df_lst:pd.DataFrame = pd.json_normalize(asdict(entry) for entry in eval(f"self.{strLabel}"))
+        df_lst: pd.DataFrame = pd.json_normalize(
+            asdict(entry) for entry in eval(f"self.{strLabel}")
+        )
         # Remove deeper nested informations, eg curves since lists in data frames are not best-practice - merge data frames if informations should be combined
-        nestedLists:List = []
-        metaInformation:List = []
+        nestedLists: List = []
+        metaInformation: List = []
         for c in df_lst:
-            c:str
-            if len(c)>3 and c[:3] == "lst":
+            c: str
+            if len(c) > 3 and c[:3] == "lst":
                 nestedLists.append(c)
-            if len(c)>5 and c[:5] == "_meta":
+            if len(c) > 5 and c[:5] == "_meta":
                 metaInformation.append(c)
         df_lst.drop(labels=nestedLists, axis="columns", inplace=True)
         # Rename '_meta' information in self.lst...
-        lstDataFrames.append(df_lst.rename(columns={k:  strLabel[3:]+"."+k.replace("_meta.","") for k in df_lst.columns}))
+        lstDataFrames.append(
+            df_lst.rename(
+                columns={
+                    k: strLabel[3:] + "." + k.replace("_meta.", "")
+                    for k in df_lst.columns
+                }
+            )
+        )
         # Merge class information and nested list information, sequence and its records
         return pd.concat(lstDataFrames, axis="columns")
 
 
 @dataclass
 class XLum_Meta(object):
-    comment:str="empty"
-    state:State=State.UNKNOWN
-    parentID:str="NA"
+    comment: str = "empty"
+    state: State = State.UNKNOWN
+    parentID: str = "NA"
 
     @classmethod
-    def from_attributes(cls, dct:Dict) -> 'XLum_Meta':
+    def from_attributes(cls, dct: Dict) -> "XLum_Meta":
         """extract meta informatino from xml attributes
 
         Args:
@@ -86,40 +111,38 @@ class XLum_Meta(object):
             print(ex)
             attr["state"] = dct["state"]
 
-        return XLum_Meta(
-            **attr
-        )
+        return XLum_Meta(**attr)
 
 
 @dataclass
 class Curve(Xlum_DataFrame_Support):
-    
-    lstValues:List=lambda: []  # internal list in the curve node
 
-    component:str="NA"
-    startDate:datetime=datetime(2013, 1, 1, 3, 42, 42, 42)
-    curveType:CurveType=CurveType.UNKNOWN
-    duration:float=0.0
-    offset:float=0.0
-    xValues:List[int]=lambda: []  # see: https://stackoverflow.com/questions/52063759/passing-default-list-argument-to-dataclasses
-    yValues:List[int]=lambda: []
-    tValues:List[int]=lambda: []
-    xLabel:str="NA"
-    yLabel:str="NA"
-    tLabel:str="NA"
-    vLabel:str="NA"
-    xUnit:str="NA"
-    yUnit:str="NA"
-    tUnit:str="NA"
-    vUnit:str="NA"
-    detectionWindow:str="NA"
-    filter:str="NA"
-    _meta:XLum_Meta=XLum_Meta(
-        comment="empty curve"
-    )
+    lstValues: List = lambda: []  # internal list in the curve node
+
+    component: str = "NA"
+    startDate: datetime = datetime(2013, 1, 1, 3, 42, 42, 42)
+    curveType: CurveType = CurveType.UNKNOWN
+    duration: float = 0.0
+    offset: float = 0.0
+    xValues: List[
+        int
+    ] = lambda: []  # see: https://stackoverflow.com/questions/52063759/passing-default-list-argument-to-dataclasses
+    yValues: List[int] = lambda: []
+    tValues: List[int] = lambda: []
+    xLabel: str = "NA"
+    yLabel: str = "NA"
+    tLabel: str = "NA"
+    vLabel: str = "NA"
+    xUnit: str = "NA"
+    yUnit: str = "NA"
+    tUnit: str = "NA"
+    vUnit: str = "NA"
+    detectionWindow: str = "NA"
+    filter: str = "NA"
+    _meta: XLum_Meta = XLum_Meta(comment="empty curve")
 
     @classmethod
-    def from_element(cls, element:etree.Element) -> 'Curve':
+    def from_element(cls, element: etree.Element) -> "Curve":
         """extract Curve information from tree element
 
         Args:
@@ -132,13 +155,28 @@ class Curve(Xlum_DataFrame_Support):
 
         attr = dict()
         # strings
-        for k in ["component", "xLabel", "yLabel", "tLabel", "vLabel", "xUnit", "yUnit", "tUnit", "vUnit", "filter", "detectionWindow"]:
+        for k in [
+            "component",
+            "xLabel",
+            "yLabel",
+            "tLabel",
+            "vLabel",
+            "xUnit",
+            "yUnit",
+            "tUnit",
+            "vUnit",
+            "filter",
+            "detectionWindow",
+        ]:
             if k in attr:
                 attr[k] = element.attrib[k]
             else:
                 attr[k] = "NA"
         # floats
-        for k in ["duration", "offset",]:
+        for k in [
+            "duration",
+            "offset",
+        ]:
             if k in attr:
                 attr[k] = float(element.attrib[k])
             else:
@@ -149,9 +187,8 @@ class Curve(Xlum_DataFrame_Support):
                 attr[k] = list(map(int, element.attrib[k].split()))
             else:
                 attr[k] = []
-        lstStrValues = re.findall(r'[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+', element.text)
+        lstStrValues = re.findall(r"[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+", element.text)
         attr["lstValues"] = list(map(float, lstStrValues))
-
 
         if "startDate" not in element.attrib:
             date = datetime(2013, 1, 1, 3, 42, 42, 42)
@@ -166,10 +203,12 @@ class Curve(Xlum_DataFrame_Support):
 
         return Curve(
             startDate=date,
-            curveType=CurveType[element.attrib["curveType"].upper().replace("-", "_")] if "curveType" in element.attrib else CurveType.UNKNOWN,
-            _meta = XLum_Meta.from_attributes(element.attrib),
-            **attr
-            )
+            curveType=CurveType[element.attrib["curveType"].upper().replace("-", "_")]
+            if "curveType" in element.attrib
+            else CurveType.UNKNOWN,
+            _meta=XLum_Meta.from_attributes(element.attrib),
+            **attr,
+        )
 
     @cached_property
     def df(self) -> pd.DataFrame:
@@ -178,34 +217,35 @@ class Curve(Xlum_DataFrame_Support):
         Returns:
             pd.DataFrame: data frame
         """
-        df:pd.DataFrame = pd.DataFrame([self]).drop(columns="_meta", axis="columns")
-        df_meta:pd.DataFrame = pd.DataFrame([self._meta]).rename(columns={c: self.__class__.__name__+"."+c for c in self._meta.__dict__})
-        metaInformation:List = []
+        df: pd.DataFrame = pd.DataFrame([self]).drop(columns="_meta", axis="columns")
+        df_meta: pd.DataFrame = pd.DataFrame([self._meta]).rename(
+            columns={c: self.__class__.__name__ + "." + c for c in self._meta.__dict__}
+        )
+        metaInformation: List = []
         lstSeriesColumns = []
         for c in df:
-            c:str
-            if len(c)>5 and c[:5] == "_meta":
+            c: str
+            if len(c) > 5 and c[:5] == "_meta":
                 metaInformation.append(c)
             if "Values" in c:
                 lstSeriesColumns.append(c)
         df = df.rename(columns={"lstValues": "curve"})
         return pd.concat([df, df_meta], axis="columns")
 
+
 @dataclass
 class Record(Xlum_DataFrame_Support):
 
-    lstCurves:List[Curve]=lambda: []
+    lstCurves: List[Curve] = lambda: []
 
-    recordType:RecordType=RecordType.UNKNOWN
-    sequenceStepNumber:int=0
-    sampleCondition:SampleCondition=SampleCondition.UNKNOWN
+    recordType: RecordType = RecordType.UNKNOWN
+    sequenceStepNumber: int = 0
+    sampleCondition: SampleCondition = SampleCondition.UNKNOWN
 
-    _meta:XLum_Meta=XLum_Meta(
-        comment="empty record"
-    )
-    
+    _meta: XLum_Meta = XLum_Meta(comment="empty record")
+
     @classmethod
-    def from_element(cls, element:etree.Element) -> 'Record':
+    def from_element(cls, element: etree.Element) -> "Record":
         """extract Record information from tree element
 
         Args:
@@ -218,30 +258,46 @@ class Record(Xlum_DataFrame_Support):
 
         meta = XLum_Meta.from_attributes(element.attrib)
         return Record(
-            lstCurves = [Curve.from_element(e) for e in element.getchildren() if e.tag.lower() == 'curve'],
-            _meta = meta,
-            recordType=RecordType[element.attrib["recordType"].upper().replace("-", "_")] if "recordType" in element.attrib else RecordType.UNKNOWN,
-            sequenceStepNumber=int(element.attrib["sequenceStepNumber"]) if "sequenceStepNumber" in element.attrib else 0,
-            sampleCondition=SampleCondition[element.attrib["sampleCondition"].upper().replace("+", "_").translate({ord(c):None for c in "()."})] if "sampleCondition" in element.attrib else SampleCondition.UNKNOWN,
+            lstCurves=[
+                Curve.from_element(e)
+                for e in element.getchildren()
+                if e.tag.lower() == "curve"
+            ],
+            _meta=meta,
+            recordType=RecordType[
+                element.attrib["recordType"].upper().replace("-", "_")
+            ]
+            if "recordType" in element.attrib
+            else RecordType.UNKNOWN,
+            sequenceStepNumber=int(element.attrib["sequenceStepNumber"])
+            if "sequenceStepNumber" in element.attrib
+            else 0,
+            sampleCondition=SampleCondition[
+                element.attrib["sampleCondition"]
+                .upper()
+                .replace("+", "_")
+                .translate({ord(c): None for c in "()."})
+            ]
+            if "sampleCondition" in element.attrib
+            else SampleCondition.UNKNOWN,
         )
+
 
 @dataclass
 class Sequence(Xlum_DataFrame_Support):
 
-    lstRecords:List[Record]=lambda: []
+    lstRecords: List[Record] = lambda: []
 
-    fileName:str="NA"
-    software:str="NA"
-    readerName:str="NA"
-    readerSN:str="NA"
-    readerFW:str="NA"
+    fileName: str = "NA"
+    software: str = "NA"
+    readerName: str = "NA"
+    readerSN: str = "NA"
+    readerFW: str = "NA"
 
-    _meta:XLum_Meta=XLum_Meta(
-        comment="empty sequence"
-    )
+    _meta: XLum_Meta = XLum_Meta(comment="empty sequence")
 
     @classmethod
-    def from_element(cls, element:etree.Element) -> 'Sequence':
+    def from_element(cls, element: etree.Element) -> "Sequence":
         """extract Sequence information from tree element
 
         Args:
@@ -255,35 +311,38 @@ class Sequence(Xlum_DataFrame_Support):
         meta = XLum_Meta.from_attributes(element.attrib)
 
         attr = {}
-        #strings
+        # strings
         for k in ["fileName", "software", "readerName", "readerSN", "readerFW"]:
             if k in element.attrib:
                 attr[k] = element.attrib[k]
 
         return Sequence(
-            lstRecords = [Record.from_element(e) for e in element.getchildren() if e.tag.lower() == 'record'],
-            _meta = meta,
+            lstRecords=[
+                Record.from_element(e)
+                for e in element.getchildren()
+                if e.tag.lower() == "record"
+            ],
+            _meta=meta,
             **attr,
         )
+
 
 @dataclass
 class Sample(Xlum_DataFrame_Support):
 
-    lstSequences:List[Sequence]=lambda: []
+    lstSequences: List[Sequence] = lambda: []
 
-    name:str="NA"
-    mineral:str="NA"
-    latitude:float=-1000.0
-    longitude:float=-1000.0
-    altitude:float=-1000.0
-    doi:str="NA"
-    
-    _meta:XLum_Meta=XLum_Meta(
-        comment="empty sample"
-    )
+    name: str = "NA"
+    mineral: str = "NA"
+    latitude: float = -1000.0
+    longitude: float = -1000.0
+    altitude: float = -1000.0
+    doi: str = "NA"
+
+    _meta: XLum_Meta = XLum_Meta(comment="empty sample")
 
     @classmethod
-    def from_element(cls, element:etree.Element) -> 'Sample':
+    def from_element(cls, element: etree.Element) -> "Sample":
         """extract Sample informatin from tree element
 
         Args:
@@ -297,19 +356,23 @@ class Sample(Xlum_DataFrame_Support):
         meta = XLum_Meta.from_attributes(element.attrib)
 
         attr = {}
-        #strings
+        # strings
         for k in ["name", "mineral", "doi"]:
             if k in element.attrib:
                 attr[k] = element.attrib[k]
-        
-        #float
+
+        # float
         for k in ["latitude", "longitude", "altitude"]:
             if k in element.attrib:
                 attr[k] = float(element.attrib[k])
 
         return Sample(
-            lstSequences = [Sequence.from_element(e) for e in element.getchildren() if e.tag.lower() == 'sequence'],
-            _meta = meta,
+            lstSequences=[
+                Sequence.from_element(e)
+                for e in element.getchildren()
+                if e.tag.lower() == "sequence"
+            ],
+            _meta=meta,
             **attr,
         )
 
@@ -317,15 +380,15 @@ class Sample(Xlum_DataFrame_Support):
 @dataclass
 class XlumMeta(Xlum_DataFrame_Support):
 
-    lstSamples:List[Sample]=lambda: []
+    lstSamples: List[Sample] = lambda: []
 
-    formatVersion:str="NA"
-    author:str="NA"
-    lang:str="NA"
-    license:str="NA"
+    formatVersion: str = "NA"
+    author: str = "NA"
+    lang: str = "NA"
+    license: str = "NA"
 
     @classmethod
-    def from_element_tree(cls, tree:etree.ElementTree) -> 'XlumMeta':
+    def from_element_tree(cls, tree: etree.ElementTree) -> "XlumMeta":
         """extract XLum meta data from an imported element tree
 
         Args:
@@ -334,17 +397,21 @@ class XlumMeta(Xlum_DataFrame_Support):
         Returns:
             XlumMeta: parsed data
         """
-        root:etree.Element = tree.getroot()
+        root: etree.Element = tree.getroot()
         assert root.tag.lower() == "xlum", f"{root.tag=} expected 'xlum'"
         for e in root.getchildren():
             print(e.tag)
 
         attr = {}
-        #strings
+        # strings
         for k in ["formatVersion", "author", "lang", "license"]:
             if k in root.attrib:
                 attr[k] = root.attrib[k]
         return cls(
-            lstSamples = [Sample.from_element(e) for e in root.getchildren() if e.tag.lower() == 'sample'],
-            **attr
+            lstSamples=[
+                Sample.from_element(e)
+                for e in root.getchildren()
+                if e.tag.lower() == "sample"
+            ],
+            **attr,
         )
--- xlum/importer.py
+++ xlum/importer.py
@@ -1,45 +1,48 @@
 import os
 from lxml import etree
 import logging
+import urllib
 
 from data.classes import XlumMeta
 
-def from_xlum(file_path:os.PathLike) -> XlumMeta:
+
+def from_xlum(file_path: os.PathLike) -> XlumMeta:
     """import data from an *.xlum file
 
     Args:
         file_path (os.PathLike): path to the file
 
     Returns:
-        XlumMeta: structured data, see: xlum.data.classes for a description of the dataclasses
-    """    
+        XlumMeta: structured data, see: xlum.data.classes for a
+                  description of the dataclasses
+    """
     assert os.path.exists(file_path), f"{file_path=} not found"
-    assert file_path.split(".")[-1].lower() == "xlum", f"{file_path.split('.')[-1]} invalid, expected '.xlum'"
-    
-    xsd_path = os.path.join(
-        os.getcwd(),
-        "../assets/xlum_schema.xsd"
-    )
+    assert (
+        file_path.split(".")[-1].lower() == "xlum"
+    ), f"{file_path.split('.')[-1]} invalid, expected '.xlum'"
 
-    with open(xsd_path) as f:
-        xsd =f.read()
+    xsd_path = os.path.join(os.getcwd(), "../assets/xlum_schema.xsd")
+    with open(xsd_path, "r") as f:
+        xsd = f.read()
 
-    xsd_source = etree.XML(
-        xsd
-    )
+    # with urllib.request.urlopen('https://github.com/R-Lum/xlum/blob/main/inst/extdata/xlum_schema.xsd') as f:  # 404 ohne Login :(
+    #     xsd = f.read().decode('utf-8')
+
+    xsd_source = etree.XML(xsd)
     schema = etree.XMLSchema(xsd_source)
-    parser = etree.XMLParser(schema = schema)
+    parser = etree.XMLParser(schema=schema)
 
     # Import XML-like data
     try:
-        tree:etree.ElementTree = etree.parse(file_path, parser)
+        tree: etree.ElementTree = etree.parse(file_path, parser)
     except etree.XMLSyntaxError as ex:
         logging.warning(type(ex), ex)
         tree = etree.parse(file_path)
     logging.debug(f"{tree}\nfrom: {file_path=}")
     return _extract_xlum(tree)
 
-def _extract_xlum(tree:etree.ElementTree) -> XlumMeta:
+
+def _extract_xlum(tree: etree.ElementTree) -> XlumMeta:
     """extract XLum meta data from an importet element tree
 
     Args:
@@ -48,6 +51,6 @@ def _extract_xlum(tree:etree.ElementTree) -> XlumMeta:
     Returns:
         XlumMeta: parsed data
     """
-    root:etree.Element = tree.getroot()
-    assert root.tag.lower() == 'xlum', f"{root.tag=}, expected 'xlum'"
+    root: etree.Element = tree.getroot()
+    assert root.tag.lower() == "xlum", f"{root.tag=}, expected 'xlum'"
     return XlumMeta.from_element_tree(tree)
